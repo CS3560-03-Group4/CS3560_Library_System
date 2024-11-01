30769daf5ebc63bc04e8856d1dc29c2eaf49a015
ann297 <anthainguyen1997@gmail.com>
2024-11-01 15:30:15 -0700

    create BookItem class

--- app/classes/BookItem.ts
+++ app/classes/BookItem.ts
@@ -1,29 +1,38 @@
 // BookItem.ts
+
 class BookItem {
+    // Order ID for the book item
     private orderID: string;
+  
+    // Book ID for the book item
     private bookID: string;
   
+    // Constructor to initialize orderID and bookID
     constructor(orderID: string, bookID: string) {
       this.orderID = orderID;
       this.bookID = bookID;
     }
-    // getter and setter
-
+  
+    // Getter for orderID
     public getOrderID(): string {
       return this.orderID;
     }
   
+    // Setter for orderID
     public setOrderID(orderID: string): void {
       this.orderID = orderID;
     }
   
+    // Getter for bookID
     public getBookID(): string {
       return this.bookID;
     }
   
+    // Setter for bookID
     public setBookID(bookID: string): void {
       this.bookID = bookID;
     }
   }
   
   export default BookItem;
+  
